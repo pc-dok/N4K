291932949fea64c8f0781df05f78e86bddf83793
pc-dok <info@n4k.at>
2019-01-15 17:41:53 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 //Note: This is only my Azure Login Credentials for my UMB Visual Studio Enterprise MPN Account
 
 provider "azurerm" {
-    subscription_id = "bb97e652-f151-4fc6-9fd7-e895ea0ae9f2"
-    client_id       = "70b786ea-0fb6-4e62-991a-90d459dd0d8b"
-    client_secret   = "f7338a89-6964-4e16-9db0-63b6dd3f6d6f"
-    tenant_id       = "aa487a7d-0a99-4ec8-9a84-2952762a0131"
+    subscription_id = ""
+    client_id       = ""
+    client_secret   = ""
+    tenant_id       = ""
 }
